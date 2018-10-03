ad364638d0387be5f820141c5ff7761c8ac8cc25
Richie Varghese <ric-v@outlook.com>
2018-10-03 23:19:16 +0530

    added get db file from UI file prompt and minor updates

--- buntDB-gui.go
+++ buntDB-gui.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/tidwall/buntdb"
@@ -32,37 +33,82 @@ var db *buntdb.DB
 var err error
 
 func main() {
+
 	// Open the data.db file. It will be created if it doesn't exist.
 	db, err = buntdb.Open("data.db")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	defer db.Close()
 
 	// Starting web server
 	http.HandleFunc("/writeData", writeData)
 	http.HandleFunc("/getAllData", getAllData)
+
+	fmt.Printf("BuntDB-GUI started at %s\n", "http://localhost:8080")
+
 	if err = http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
 
 // Function to write data
 func writeData(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Println("Page hit : /writeData")
+
+	var key, value string
+
+	// open the specified DB
+	if dbPath, _ := r.URL.Query()["db"]; len(dbPath) > 1 {
+		db, err = buntdb.Open(dbPath[0])
+		// log error
+		if err != nil {
+			log.Println(err)
+		}
+		defer db.Close()
+	}
+
+	// get key
+	if keys, _ := r.URL.Query()["key"]; len(keys) > 1 {
+		key = keys[0]
+	}
+
+	// get value
+	if values, _ := r.URL.Query()["value"]; len(values) > 1 {
+		value = values[0]
+	}
+
 	// Write data
 	err = db.Update(func(tx *buntdb.Tx) error {
-		_, _, err := tx.Set("key1", "value1", nil)
+		_, done, err := tx.Set(key, value, nil)
+		if done {
+			w.Write([]byte("Successfully set new value to DB"))
+		} else {
+			w.Write([]byte("write failed"))
+		}
 		return err
 	})
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
+
 }
 
 // Function to read data
 func getAllData(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Page hit : /getAllData")
+
+	// open the specified DB
+	if dbPath, _ := r.URL.Query()["db"]; len(dbPath) > 1 {
+		db, err = buntdb.Open(dbPath[0])
+		// log error
+		if err != nil {
+			log.Println(err)
+		}
+		defer db.Close()
+	}
+
 	// Reset data
 	dbContent = nil
 	// Open a view transaction
@@ -75,20 +121,20 @@ func getAllData(w http.ResponseWriter, r *http.Request) {
 		return err
 	})
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	// Marshal into string
 	jsondata, err := json.Marshal(dbContent)
 	fmt.Println(string(jsondata))
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	// Data into response structure
 	response.Body = string(jsondata)
 	// Marshal response
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	// Sending response
 	w.Write(jsonResponse)
